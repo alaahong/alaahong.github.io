a0e5e645f441b6122a4711d113e63101dc657d40
ian.zhang <ian.zhangzhe@gmail.com>
2022-02-08 14:20:28 +0800

    Publishing to github

--- temp/chrome.js
+++ temp/chrome.js
@@ -3,5 +3,6 @@ shadowRoot1.innerHTML = `
               <input id="actual-keyin" placeholder="yourname">
             `
 let exp = ['+','-','*','/']
+let mark = exp[Math.floor(Math.random() * 3)]
 document.querySelector("body > div:nth-child(2) > div:nth-child(2)").id= ("id-" + Math.floor(Math.random() * 11))
-document.querySelector("body > div:nth-child(2) > div > p").innerText = ((Math.floor(Math.random() * 11)+1) +exp[((Math.floor(Math.random() * 3))] + (Math.floor(Math.random() * 11)+1))
+document.querySelector("body > div:nth-child(2) > div > p").innerText = ((Math.floor(Math.random() * 11)+1) + mark + (Math.floor(Math.random() * 11)+1))
