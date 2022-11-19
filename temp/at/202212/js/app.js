d3dd09cf1b003a706f4153b0eff682decfc523b8
ian.zhang <ian.zhangzhe@gmail.com>
2022-11-19 14:55:18 +0800

    Publishing to github

--- temp/at/202212/js/app.js
+++ temp/at/202212/js/app.js
@@ -1,22 +1,38 @@
 const App = {
   template: '#app-template',
   data: () => ({
+        registerFormValid: true,
+        username: null,
+        password: null,
+        emptyRules:[v=>!!v||'field is required'],
+        emptyArrayRules:[
+        v=>!!v||'please select two options at least',
+        v=>(v && v.length>1)|| 'please select two options at least'
+        ],
         loading:{
-        loadingRegister: false,
+        loadingRegister: true,
         loadingLogin: true,
-        displayLogin:true,
+        loadingInfo: true,
         displayRegister:false,
+        displayLogin:true,
+        displayInfo:false,
         },
+        loginDivGroupId: 'login-div-group-1',
+        loginBtnGroupId: 'login-btn-group-1',
+        infoDivGroupId: 'info-btn-group-1',
+        loginBtnId: 'login-btn-id-1',
+        registerBtnId: 'register-btn-id-1',
+        stepContent:'Step 1: Click register button',
         loginForm:{
             name:null,
-            pwd: null,
+            pwd: 'defaultpwd',
         },
         formData:{
             name:null,
-            pwd:null,
+            pwd:'changeme',
             pic:null,
-            sexual:null,
-            topic:[],
+            gender:null,
+            topic:null,
         },
         topicItem:['Java','C#',"Python","JavaScript","Go","Ruby","Groovy","Rust"]
     }),
@@ -31,22 +47,65 @@ const App = {
     showLoginContent: function(){
     this.loading.displayLogin =true
     this.loading.displayRegister =false
+    this.loading.displayInfo = false
         setTimeout(()=>{
        this.loading.loadingLogin =false
-       this.loading.loadingRegister = (true)
-    }, Math.floor((Math.random()*3000)+2000));
+       this.loading.loadingRegister = true
+       this.loading.loadingInfo = true
+       let number = parseInt(Math.floor((Math.random()*10)+0))
+       this.loginDivGroupId = 'login-group-id-'+ parseInt(number)
+       this.loginBtnGroupId = 'login-group-id-'+ parseInt(number+1)
+       this.infoDivGroupId = 'info-group-id-'+ parseInt(number+2)
+       this.loginBtnId =    'login-btn-id-'     + parseInt(number)
+       this.registerBtnId = 'login-btn-id-'  + parseInt(number+1)
+       this.info
+    }, Math.floor((Math.random()*3000)+2000))
     },
+    changeLoginFormId: function(){
+        },
     showRegister(){
     this.loading.displayLogin =false
     this.loading.displayRegister =true
+    this.loading.displayInfo = false
         setTimeout(()=>{
        this.loading.loadingLogin = true
        this.loading.loadingRegister = false
-    }, Math.floor((Math.random()*3000)+2000));
+       this.loading.loadingInfo = true
+    }, Math.floor((Math.random()*3000)+2000))
+    this.stepContent = 'Step 2: Fill required fields then submit'
     },
-    showLogin(){
+    login(){
+        if(this.username == null || (this.username != this.formData.name)){
+            alert('username is invalid')
+        }else if(this.password == null || (this.password != this.formData.pwd)){
+            alert('password is invalid')
+        }else{
+        this.loading.displayLogin = false
+        this.loading.displayRegister = false
+        this.loading.displayInfo = true
+        setTimeout(()=>{
+        this.loading.loadingLogin = true
+        this.loading.loadingRegister = true
+        this.loading.loadingInfo = false
+    }, Math.floor((Math.random()*3000)+2000))
+        this.stepContent = 'Step 4: Assert username is same as yours'
+        }
+    },
+    validateForm(){
+    let formValidated = this.$refs.registerForm.validate()
+    if(formValidated){
     this.loading.displayLogin = true
     this.loading.displayRegister = false
+    this.loading.displayInfo = false
+        setTimeout(()=>{
+       this.loading.loadingLogin = false
+       this.loading.loadingInfo = true
+       this.loading.loadingRegister = true
+    }, Math.floor((Math.random()*3000)+2000))
+    this.stepContent = 'Step 3: Login'
+    this.username = this.formData.name
+    this.password = this.formData.pwd
+    }
     },
     }
 }
